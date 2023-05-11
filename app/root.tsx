b53bb8dfa7334400bd7161243d50ce8e436daac2
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2023-05-11 15:19:17 +0200

    Første route, starter å fikse dekoratøren

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json, LinksFunction, LoaderArgs } from '@remix-run/node';
+import type { LoaderArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,19 +8,21 @@ import {
   ScrollRestoration,
   useLoaderData,
   useRouteError,
-} from '@remix-run/react';
-import navStyles from '@navikt/ds-css/dist/index.css';
-import { cssBundleHref } from '@remix-run/css-bundle';
-import { getEnv } from './utils/env.utils';
-import { RootErrorBoundaryView } from './components/error-boundary/RootErrorBoundaryView';
+} from "@remix-run/react";
+import navStyles from "@navikt/ds-css/dist/index.css";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import { getEnv } from "./utils/env.utils";
+import { RootErrorBoundaryView } from "./components/error-boundary/RootErrorBoundaryView";
+import { fetchDecoratorReact } from "@navikt/nav-dekoratoren-moduler/ssr";
+import type { DecoratorComponents } from "@navikt/nav-dekoratoren-moduler/ssr";
 
 export function meta() {
   return [
     {
-      charset: 'utf-8',
-      title: 'Dagpenger rapportering',
-      viewport: 'width=device-width,initial-scale=1',
-      description: 'rapporteringløsning for dagpenger',
+      charset: "utf-8",
+      title: "Dagpenger rapportering",
+      viewport: "width=device-width,initial-scale=1",
+      description: "rapporteringløsning for dagpenger",
     },
   ];
 }
@@ -29,31 +31,31 @@ export function links() {
   return [
     ...(cssBundleHref
       ? [
-          { rel: 'stylesheet', href: cssBundleHref },
-          { rel: 'stylesheet', href: navStyles },
+          { rel: "stylesheet", href: cssBundleHref },
+          { rel: "stylesheet", href: navStyles },
           {
-            rel: 'apple-touch-icon',
-            sizes: '180x180',
-            href: `${getEnv('BASE_PATH')}/apple-touch-icon.png`,
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: `${getEnv("BASE_PATH")}/apple-touch-icon.png`,
           },
           {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '32x32',
-            href: `${getEnv('BASE_PATH')}/favicon-32x32.png`,
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: `${getEnv("BASE_PATH")}/favicon-32x32.png`,
           },
           {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '16x16',
-            href: `${getEnv('BASE_PATH')}/favicon-16x16.png`,
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: `${getEnv("BASE_PATH")}/favicon-16x16.png`,
           },
-          { rel: 'manifest', href: `${getEnv('BASE_PATH')}/site.webmanifest` },
-          { rel: 'manifest', href: `${getEnv('BASE_PATH')}/site.webmanifest` },
+          { rel: "manifest", href: `${getEnv("BASE_PATH")}/site.webmanifest` },
+          { rel: "manifest", href: `${getEnv("BASE_PATH")}/site.webmanifest` },
           {
-            rel: 'mask-icon',
-            href: `${getEnv('BASE_PATH')}/safari-pinned-tab.svg`,
-            color: '#5bbad5',
+            rel: "mask-icon",
+            href: `${getEnv("BASE_PATH")}/safari-pinned-tab.svg`,
+            color: "#5bbad5",
           },
         ]
       : []),
@@ -61,20 +63,39 @@ export function links() {
 }
 
 export async function loader({ request }: LoaderArgs) {
-  return json({
-    env: {},
+  const dekoratorEnv = "dev";
+
+  const Decorator: DecoratorComponents = await fetchDecoratorReact({
+    env: dekoratorEnv ?? "prod",
+    params: {
+      language: "nb",
+      context: "privatperson",
+      chatbot: false,
+      simpleHeader: true,
+      enforceLogin: false,
+      redirectToApp: true,
+      level: "Level4",
+    },
   });
+
+  console.log("Decorator i loader", Decorator);
+
+  return {
+    env: {},
+    Decorator,
+  };
 }
 
 export default function App() {
-  const { env } = useLoaderData<typeof loader>();
+  const { env, Decorator } = useLoaderData<typeof loader>();
 
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+
         <Links />
       </head>
       <body>
@@ -95,5 +116,7 @@ export default function App() {
 export function ErrorBoundary() {
   const error = useRouteError();
 
-  return <RootErrorBoundaryView links={<Links />} meta={<Meta />} error={error} />;
+  return (
+    <RootErrorBoundaryView links={<Links />} meta={<Meta />} error={error} />
+  );
 }
