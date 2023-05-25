e5c529f2b48ded8049185fb305ae0a455f92e362
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-05-25 14:00:29 +0200

    Flytt datoformattering til utils og skriv tester

---
+++ app/utils/dato.utils.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect } from "vitest";
+import { formaterPeriodeDato, formaterPeriodeTilUkenummer } from "./dato.utils";
+
+describe('formaterPeriodeDato', () => {
+  test('returnerer riktig periodeformat', () => {
+    const fraOgMed = "2023-05-22";
+    const tilOgMed = "2023-06-04";
+
+    const periodeString = formaterPeriodeDato(fraOgMed, tilOgMed)
+
+    expect(periodeString).toBe("22.05.2023 - 04.06.2023")
+  });
+})
+
+describe('formaterPeriodeTilUkenummer', () => {
+  test('returnerer riktig periodeukenummer', () => {
+    const fraOgMed = "2023-05-22";
+    const tilOgMed = "2023-06-04";
+
+    const periodeString = formaterPeriodeTilUkenummer(fraOgMed, tilOgMed)
+
+    expect(periodeString).toBe("21 - 22")
+  });
+})
