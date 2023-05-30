ac211430fa6411f66109bb6b50a8474f840bedbc
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-05-30 16:38:04 +0200

    Bruker process.env istedenfor getEnv på auth.utils.server

--- app/utils/auth.utils.server.ts
+++ app/utils/auth.utils.server.ts
@@ -5,7 +5,6 @@ import {
 } from "@navikt/dp-auth";
 import { idporten } from "@navikt/dp-auth/identity-providers";
 import { tokenX, withInMemoryCache } from "@navikt/dp-auth/obo-providers";
-import { getEnv } from "./env.utils";
 
 const fallbackToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
@@ -27,10 +26,10 @@ if (process.env.IS_LOCALHOST === "true") {
 }
 
 export async function getRapporteringOboToken(session: SessionWithOboProvider) {
-  if (getEnv("IS_LOCALHOST") === "true" && getEnv("DP_RAPPORTERING_TOKEN")) {
-    return getEnv("DP_RAPPORTERING_TOKEN");
+  if (process.env.IS_LOCALHOST === "true") {
+    return process.env.DP_RAPPORTERING_TOKEN || fallbackToken;
   } else {
-    const audienceDPRapportering = `${getEnv("NAIS_CLUSTER_NAME")}:teamdagpenger:dp-rapportering`;
+    const audienceDPRapportering = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-rapportering`;
     return session.apiToken(audienceDPRapportering);
   }
 }
