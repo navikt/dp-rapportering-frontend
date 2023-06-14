f24b6eaf338aef7dafb21bef869830bf0d01e0b1
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-06-14 15:07:07 +0200

    Legge til bedre tall validering for arbeidstimer

--- app/utils/validering.util.ts
+++ app/utils/validering.util.ts
@@ -7,10 +7,17 @@ export const aktivitetsvalideringArbeid = z.object({
   dato: z.coerce.date({
     invalid_type_error: "Ugyldig dato",
   }),
-  timer: z
-    .string()
-    .nonempty({ message: "Du må skrive et tall" })
-    .regex(new RegExp("^\\d*(,)?\\d*$"), "Det må være et gyldig tall"), // Regex for å matche tall med komma seperator
+  timer: z.preprocess(
+    (val) => String(val).replace(/,/g, "."),
+    z.coerce
+      .number({
+        required_error: "Du må skrive et tall",
+        invalid_type_error: "Det må være et gyldig tall",
+      })
+      .positive({ message: "Arbeidstime kan ikke være et nagativt" })
+      .min(0.5, { message: "Arbeidstime må være mer enn 0,5 timer" })
+      .max(24, { message: "Arbeidstime være mellom 0,5 og 24 timer" })
+  ),
 });
 
 export const aktivitetsvalideringSykFerie = aktivitetsvalideringArbeid.partial({ timer: true });
