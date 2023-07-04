e95b093e36b60dfadc42f93733af1d60c9913076
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-07-04 10:21:35 +0200

    Lagt til vis-alle fil

---
+++ app/routes/rapportering.vis-alle.tsx
@@ -0,0 +1,134 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
+import { Alert, Button, Heading, Modal } from "@navikt/ds-react";
+import { type ActionArgs } from "@remix-run/node";
+import { Form, useActionData, useRouteLoaderData } from "@remix-run/react";
+import { isFriday, isPast, isToday } from "date-fns";
+import { useEffect, useState } from "react";
+import { RemixLink } from "~/components/RemixLink";
+import { AktivitetModal } from "~/components/aktivitet-modal/AktivitetModal";
+import { AktivitetOppsummering } from "~/components/aktivitet-oppsummering/AktivitetOppsummering";
+import { Kalender } from "~/components/kalender/Kalender";
+import { useSanity } from "~/hooks/useSanity";
+import { type IRapporteringLoader } from "./rapportering";
+import { lagreAktivitetAction, slettAktivitetAction } from "~/utils/aktivitet.action.server";
+import { type IRapporteringsperiode } from "~/models/rapporteringsperiode.server";
+
+import styles from "./rapportering.module.css";
+
+export async function action({ request }: ActionArgs) {
+  const formdata = await request.formData();
+  const submitKnapp = formdata.get("submit");
+
+  switch (submitKnapp) {
+    case "slette": {
+      return await slettAktivitetAction(formdata, request);
+    }
+
+    case "lagre": {
+      return await lagreAktivitetAction(formdata, request);
+    }
+  }
+}
+
+export default function Rapportering() {
+  const { rapporteringsperiode, allePerioder } = useRouteLoaderData(
+    "routes/rapportering"
+  ) as IRapporteringLoader;
+  const actionData = useActionData();
+
+  const [valgtDato, setValgtDato] = useState<string | undefined>(undefined);
+  const [modalAapen, setModalAapen] = useState(false);
+  const { hentAppTekstMedId } = useSanity();
+
+  useEffect(() => {
+    Modal.setAppElement("#dp-rapportering-frontend");
+  }, []);
+
+  useEffect(() => {
+    if (actionData?.lagret) {
+      lukkModal();
+    }
+  }, [actionData]);
+
+  function aapneModal(dato: string) {
+    setValgtDato(dato);
+    setModalAapen(true);
+  }
+
+  function lukkModal() {
+    setValgtDato(undefined);
+    setModalAapen(false);
+  }
+
+  // Vet ikke om det er slik det skal være, vi må finne ut av det
+  // Her burde det være en del av periode responsen fra backend
+  // {   .... klarForGodkjenning: boolean;}
+  function kanGodkjenne(): boolean {
+    // Hente ut siste fredag fra gjeldende periode
+    const sisteFredag = rapporteringsperiode.dager
+      .filter((dag) => isFriday(new Date(dag.dato)))
+      .slice(-1)[0];
+
+    // Bruk f.eks denne om du ønsker å teste en annen dag. Eks første mandag i rapporteringsperioden
+    // const sisteFredag = rapporteringsperiode.dager.filter((dag) => isMonday(new Date(dag.dato)))[0];
+    return isToday(new Date(sisteFredag.dato)) || isPast(new Date(sisteFredag.dato));
+  }
+
+  return (
+    <>
+      <Heading level="2" size="large" spacing>
+        {hentAppTekstMedId("rapportering-periode-tittel")}
+      </Heading>
+
+      {rapporteringsperiode.status === "Godkjent" && (
+        <Alert variant="success" className="my-6">
+          Du har sendt inn meldekortet! Du trenger ikke gjøre noe mer :)
+        </Alert>
+      )}
+
+      <Kalender rapporteringsperiode={rapporteringsperiode} aapneModal={aapneModal} />
+
+      <AktivitetModal
+        rapporteringsperiode={rapporteringsperiode}
+        valgtDato={valgtDato}
+        modalAapen={modalAapen}
+        lukkModal={lukkModal}
+      />
+
+      <AktivitetOppsummering rapporteringsperiode={rapporteringsperiode} />
+
+      <div className={styles.navigasjonKontainer}>
+        <RemixLink to="" as="Button" variant="secondary" icon={<ArrowLeftIcon fontSize="1.5rem" />}>
+          Mine side
+        </RemixLink>
+        <RemixLink
+          to={
+            // kanGodkjenne() ? "/rapportering/send-inn" : "/rapportering/send-inn-ikke-tilgjengelig"
+            true ? "/rapportering/send-inn" : "/rapportering/send-inn-ikke-tilgjengelig"
+          }
+          as="Button"
+          variant="primary"
+          icon={<ArrowRightIcon fontSize="1.5rem" />}
+          iconPosition="right"
+        >
+          Neste steg
+        </RemixLink>
+      </div>
+
+      <ul>
+        {allePerioder &&
+          allePerioder.map((periode: IRapporteringsperiode) => (
+            <li key={periode.id}>
+              {periode.fraOgMed} {periode.tilOgMed} - {periode.status} ({periode.id})
+              <Form method="post">
+                <input type="hidden" name="periode-id" value={periode.id}></input>
+                <Button type="submit" name="submit" value="start-korrigering">
+                  Korriger
+                </Button>
+              </Form>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
