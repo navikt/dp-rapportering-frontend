0b17570ada1eab9d719471b2dff700ff3f8cb548
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-07-25 14:53:38 +0200

    Splitter Kalender komponent til mindre deler, refakturering

---
+++ app/components/kalender/RedigeringsLenke.tsx
@@ -0,0 +1,40 @@
+import { RemixLink } from "../RemixLink";
+import styles from "./Kalender.module.css";
+
+interface IProps {
+  id: string;
+  status: string;
+}
+
+interface IRedigeringsLenke {
+  sti: string;
+  tekst: string;
+}
+
+export function RedigeringsLenke(props: IProps) {
+  function finnRedigeringsType(status: string): IRedigeringsLenke {
+    switch (status) {
+      case "TilUtfylling":
+        return { sti: "fyllut", tekst: "Fyll ut" };
+      case "Godkjent":
+        return { sti: "avgodkjenn", tekst: "Avgodkjenn" };
+      case "Innsendt":
+        return { sti: "korriger", tekst: "Korriger" };
+      default:
+        console.log("Traff ukjent status i kalenderlenke: ", status);
+        return { sti: "", tekst: "Ukjent" };
+    }
+  }
+
+  const redigeringsType = finnRedigeringsType(props.status);
+
+  return (
+    <RemixLink
+      as="Link"
+      to={`/rapportering/periode/${props.id}/${redigeringsType.sti}`}
+      className={styles.kalenderHeaderPeriodeAlternativer}
+    >
+      {redigeringsType.tekst}
+    </RemixLink>
+  );
+}
