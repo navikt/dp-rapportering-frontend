60150ac63d8da9b7393a7a57402f7a4c9bad5d68
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-08-10 15:12:25 +0200

    Tester prettier import sort

--- app/routes/rapportering.korriger.$rapporteringsperiodeId.fyllut.tsx
+++ app/routes/rapportering.korriger.$rapporteringsperiodeId.fyllut.tsx
@@ -1,15 +1,15 @@
 import { BodyLong, Heading, Modal } from "@navikt/ds-react";
 import type { ActionArgs } from "@remix-run/node";
 import { useActionData, useRouteLoaderData } from "@remix-run/react";
+import { RemixLink } from "~/components/RemixLink";
 import { useEffect, useState } from "react";
 import invariant from "tiny-invariant";
-import { RemixLink } from "~/components/RemixLink";
-import { AktivitetModal } from "~/components/aktivitet-modal/AktivitetModal";
 import { AktivitetOppsummering } from "~/components/aktivitet-oppsummering/AktivitetOppsummering";
 import { Kalender } from "~/components/kalender/Kalender";
+import { AktivitetModal } from "~/components/aktivitet-modal/AktivitetModal";
+import styles from "~/routes-styles/rapportering.module.css";
 import type { TAktivitetType } from "~/models/aktivitet.server";
 import type { IRapporteringsperiodeDag } from "~/models/rapporteringsperiode.server";
-import styles from "~/routes-styles/rapportering.module.css";
 import type { IRapporteringsPeriodeLoader } from "~/routes/rapportering.periode.$rapporteringsperiodeId";
 import { lagreAktivitetAction, slettAktivitetAction } from "~/utils/aktivitet.action.server";
 
