bc0bd0b67b2b59abf1372bfbc30734338e7e83ca
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-08-22 10:45:22 +0200

    NAV-14532 Lagt inn header

---
+++ app/routes/rapportering.periode.$rapporteringsperiodeId.info.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "@navikt/ds-react";
+
+export default function Info() {
+  return (
+    <>
+      <main className="rapportering-kontainer">
+        <Heading size={"medium"} level={"2"}>
+          Hva skal jeg rapportere til NAV?
+        </Heading>
+      </main>
+    </>
+  );
+}
