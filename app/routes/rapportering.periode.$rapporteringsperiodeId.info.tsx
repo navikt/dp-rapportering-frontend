548c5f6ed8be385edfe55f9a0adabcb2b8ff397d
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-08-22 10:49:31 +0200

    NAV-14532 Lagt inn accordian med overskrifter, mangler innhold

--- app/routes/rapportering.periode.$rapporteringsperiodeId.info.tsx
+++ app/routes/rapportering.periode.$rapporteringsperiodeId.info.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@navikt/ds-react";
+import { Accordion, BodyLong, Heading } from "@navikt/ds-react";
 
 export default function Info() {
   return (
@@ -7,6 +7,28 @@ export default function Info() {
         <Heading size={"medium"} level={"2"}>
           Hva skal jeg rapportere til NAV?
         </Heading>
+        <BodyLong spacing>
+          Her kan du lese om hva du skal rapportere til NAV når du mottar dagpenger. Det er lurt å
+          gjøre seg godt kjent med hvordan rapporteringen fungerer.
+        </BodyLong>
+        <Accordion>
+          <Accordion.Item>
+            <Accordion.Header>Hva må jeg rapportere som arbeid?</Accordion.Header>
+            <Accordion.Content>LEGG INN INNHOLD</Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item>
+            <Accordion.Header>Hva må jeg rapportere av tiltak, kurs og utdanning?</Accordion.Header>
+            <Accordion.Content>LEGG INN INNHOLD</Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item>
+            <Accordion.Header>Hva gjelder for sykdom?</Accordion.Header>
+            <Accordion.Content>LEGG INN INNHOLD</Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item>
+            <Accordion.Header>Hvis jeg tar ferie eller har annet fravær?</Accordion.Header>
+            <Accordion.Content>LEGG INN INNHOLD</Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
       </main>
     </>
   );
