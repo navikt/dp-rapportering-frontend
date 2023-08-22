8f6dcef59d937ae8395f8f8bfb34a394d03e8f7e
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-08-22 09:59:38 +0200

    NAV-14530 Fjernet Beregnes etter, reversert

--- app/components/kalender/PeriodeHeaderDetaljer.tsx
+++ app/components/kalender/PeriodeHeaderDetaljer.tsx
@@ -1,14 +1,16 @@
 import type { IRapporteringsperiode } from "~/models/rapporteringsperiode.server";
 import { formaterPeriodeDato, formaterPeriodeTilUkenummer } from "~/utils/dato.utils";
 import { Heading } from "@navikt/ds-react";
+import { format } from "date-fns";
 import styles from "./PeriodeHeaderDetaljer.module.css";
+import nbLocale from "date-fns/locale/nb";
 
 interface IProps {
   rapporteringsperiode: IRapporteringsperiode;
 }
 
 export function PeriodeHeaderDetaljer({ rapporteringsperiode }: IProps) {
-  const { fraOgMed, tilOgMed } = rapporteringsperiode;
+  const { fraOgMed, tilOgMed, beregnesEtter, status } = rapporteringsperiode;
 
   return (
     <>
@@ -16,6 +18,11 @@ export function PeriodeHeaderDetaljer({ rapporteringsperiode }: IProps) {
         {`Uke ${formaterPeriodeTilUkenummer(fraOgMed, tilOgMed)}`}
       </Heading>
       <span className="tekst-subtil">{formaterPeriodeDato(fraOgMed, tilOgMed)}</span>
+      {(status === "TilUtfylling" || status === "Godkjent") && (
+        <p className="tekst-subtil">
+          Beregnes etter: {format(new Date(beregnesEtter), "EEEE d. MMMM", { locale: nbLocale })}
+        </p>
+      )}
     </>
   );
 }
