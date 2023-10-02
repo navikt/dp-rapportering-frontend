0254948d0c82ce5af970873c5fc1ff7bb8dd1e74
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-10-02 13:09:16 +0200

    Legge til undefine sjekk for useTypedRouteLoader hook

--- app/hooks/useTypedRouteLoaderData.ts
+++ app/hooks/useTypedRouteLoaderData.ts
@@ -1,9 +1,8 @@
 import { useRouteLoaderData } from "@remix-run/react";
-import type { SerializeFrom } from "@remix-run/node";
 import type { loader as RootLoader } from "~/root";
 import type { loader as RouteRapportering } from "~/routes/rapportering";
-import type { loader as RouteRapporteringPeriodeRapporteringsId } from "~/routes/rapportering.periode.$rapporteringsperiodeId";
 import type { loader as RouteRapporteringKorringeringRapporteringsId } from "~/routes/rapportering.korriger.$rapporteringsperiodeId";
+import type { loader as RouteRapporteringPeriodeRapporteringsId } from "~/routes/rapportering.periode.$rapporteringsperiodeId";
 
 type Loaders = {
   root: typeof RootLoader;
@@ -13,5 +12,13 @@ type Loaders = {
 };
 
 export function useTypedRouteLoaderData<T extends keyof Loaders>(route: T) {
-  return useRouteLoaderData(route) as SerializeFrom<Loaders[T]>;
+  const routeData = useRouteLoaderData<Loaders[T]>(route);
+
+  if (!routeData) {
+    throw new Error(
+      "Route data is not loaded. You might be trying to accessing data from a sub route that has not yet loaded"
+    );
+  }
+
+  return routeData;
 }
