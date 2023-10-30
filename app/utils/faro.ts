2798bb1891cc0f7a7346fdf44d3ee421d801ec23
Knut Riise <kmriise@users.noreply.github.com>
2023-10-30 11:58:36 +0100

    Observability/faro (#47)
    * legger på grafana faro for web vitals
    * fjerner browser env url'er som kun skal trenges serverside
    * legger til codeowners

---
+++ app/utils/faro.ts
@@ -0,0 +1,26 @@
+import { type Faro, getWebInstrumentations, initializeFaro } from "@grafana/faro-web-sdk";
+import { getEnv } from "~/utils/env.utils";
+let faro: Faro | null = null;
+
+export function initInstrumentation(): void {
+  if (typeof window === "undefined" || faro !== null) return;
+
+  getFaro();
+}
+
+export function getFaro(): Faro | null {
+  if (faro != null) return faro;
+  faro = initializeFaro({
+    url: getEnv("FARO_URL"), // required, see below
+    app: {
+      name: "dp-rapportering-frontend", // required
+      version: "0.1", // optional; useful in Grafana to get diff between versions
+    },
+    instrumentations: [
+      ...getWebInstrumentations({
+        captureConsole: true,
+      }),
+    ],
+  });
+  return faro;
+}
