b3ecf873bc0ecc91ca2ce11035e5fc28fd4fa387
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-02-22 10:24:58 +0100

    Liten refakturering av auth.util

--- app/utils/auth.utils.server.ts
+++ app/utils/auth.utils.server.ts
@@ -4,6 +4,7 @@ const fallbackToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
 const localToken = process.env.DP_RAPPORTERING_TOKEN ?? fallbackToken;
+const audienceDPRapportering = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-rapportering`;
 
 export function sessionExpiresIn(request: Request) {
   const token = process.env.IS_LOCALHOST === "true" ? localToken : getToken(request);
@@ -18,8 +19,6 @@ export function sessionExpiresIn(request: Request) {
   }
 }
 
-const audienceDPRapportering = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-rapportering`;
-
 export async function getRapporteringOboToken(request: Request) {
   if (process.env.IS_LOCALHOST === "true") {
     if (sessionExpiresIn(request) <= 0 && process.env.USE_MSW !== "true") {
@@ -30,21 +29,22 @@ export async function getRapporteringOboToken(request: Request) {
     }
 
     return localToken;
-  } else {
-    const token = getToken(request);
-    if (!token) {
-      throw new Response(null, { status: 500, statusText: "Feil ved henting av token" });
-    }
-
-    const validation = await validateToken(token);
-    if (!validation.ok) {
-      throw new Response(null, { status: 500, statusText: "Feil ved validering av token" });
-    }
-
-    const obo = await requestOboToken(token, audienceDPRapportering);
-    if (!obo.ok) {
-      throw new Response(null, { status: 500, statusText: "Feil ved henting av obo token" });
-    }
-    return obo.token;
   }
+
+  const token = getToken(request);
+  if (!token) {
+    throw new Response(null, { status: 500, statusText: "Feil ved henting av token" });
+  }
+
+  const validation = await validateToken(token);
+  if (!validation.ok) {
+    throw new Response(null, { status: 500, statusText: "Feil ved validering av token" });
+  }
+
+  const obo = await requestOboToken(token, audienceDPRapportering);
+  if (!obo.ok) {
+    throw new Response(null, { status: 500, statusText: "Feil ved henting av obo token" });
+  }
+
+  return obo.token;
 }
