1f73ddef8400ae2875420e1ae3c9cb0ec6b8a9e8
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-05-23 09:43:56 +0200

    Legg til alias i vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import path from "path";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, "tests/*"],
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
   },
 });
