1ca76cae86d88a7193ca1f1f433f3932ff02f7b3
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-07-02 09:09:48 +0200

    Endre tekst til redirekt-knappen i bekreftelseside

--- app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
+++ app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
@@ -1,11 +1,27 @@
 import { Alert, BodyLong, Heading } from "@navikt/ds-react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { hentRapporteringsperioder } from "~/models/rapporteringsperiode.server";
 import { useSanity } from "~/hooks/useSanity";
 import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import { RemixLink } from "~/components/RemixLink";
 import { AktivitetOppsummering } from "~/components/aktivitet-oppsummering/AktivitetOppsummering";
 import { Kalender } from "~/components/kalender/Kalender";
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const rapporteringsperioderResponse = await hentRapporteringsperioder(request);
+
+  if (rapporteringsperioderResponse.ok) {
+    const rapporteringsperioder = await rapporteringsperioderResponse.json();
+    const harNestePeriode = rapporteringsperioder.length > 0;
+    return json({ harNestePeriode });
+  }
+
+  return json({ harNestePeriode: false });
+}
+
 export default function RapporteringsPeriodesBekreftelsesSide() {
+  const { harNestePeriode } = useLoaderData<typeof loader>();
   const { periode } = useTypedRouteLoaderData("routes/periode.$rapporteringsperiodeId");
   const { getAppText, getLink } = useSanity();
 
@@ -31,7 +47,9 @@ export default function RapporteringsPeriodesBekreftelsesSide() {
           to={getLink("rapportering-periode-bekreftelse-tilbake").linkUrl}
           className="py-4 px-8"
         >
-          {getLink("rapportering-periode-bekreftelse-tilbake").linkText}
+          {harNestePeriode
+            ? getLink("rapportering-periode-bekreftelse-neste").linkText
+            : getLink("rapportering-periode-bekreftelse-tilbake").linkText}
         </RemixLink>
       </div>
     </div>
