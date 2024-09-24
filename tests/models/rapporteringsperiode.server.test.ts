254052c9515795530efd22dd7ccc3cc8faa2a6c1
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-09-24 12:34:14 +0200

    Skriv tester for rapporteringsperiode.server

---
+++ tests/models/rapporteringsperiode.server.test.ts
@@ -0,0 +1,65 @@
+import { HttpResponse, http } from "msw";
+import { beforeEach } from "node:test";
+import { afterAll, beforeAll, describe, expect, test, vi } from "vitest";
+import { lagRapporteringsperiode } from "~/devTools/rapporteringsperiode";
+import { hentRapporteringsperioder } from "~/models/rapporteringsperiode.server";
+import { server } from "../../mocks/server";
+
+const url = `${process.env.DP_RAPPORTERING_URL}/rapporteringsperioder`;
+
+vi.mock("~/utils/fetch.utils", () => ({
+  getHeaders: vi.fn(() => ({
+    "Content-Type": "application/json",
+    Accept: "application",
+    Authorization: "Bearer token",
+  })),
+}));
+
+beforeEach(() => server.resetHandlers());
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterAll(() => server.close());
+
+describe("rapporteringsperiode.server", () => {
+  const request = new Request(url);
+
+  const rapporteringsperioder = [lagRapporteringsperiode()];
+  test("henter rapporteringsperioder", async () => {
+    server.use(
+      http.get(url, () => {
+        return HttpResponse.json(rapporteringsperioder, { status: 200 });
+      })
+    );
+
+    expect(await hentRapporteringsperioder(request)).toEqual(rapporteringsperioder);
+  });
+
+  test("ingen rapportering funnet", async () => {
+    server.use(
+      http.get(url, () => {
+        return new HttpResponse(null, { status: 204 });
+      })
+    );
+
+    try {
+      await hentRapporteringsperioder(request);
+    } catch (error) {
+      expect((error as Response).status).toBe(404);
+      expect(await (error as Response).text()).toBe("Ingen meldekort funnet");
+    }
+  });
+
+  test("henting av rapporteringsperioder feiler", async () => {
+    server.use(
+      http.get(url, () => {
+        return new HttpResponse(null, { status: 500 });
+      })
+    );
+
+    try {
+      await hentRapporteringsperioder(request);
+    } catch (error) {
+      expect((error as Response).status).toBe(500);
+      expect(await (error as Response).text()).toBe("Feil i uthenting av meldekort");
+    }
+  });
+});
