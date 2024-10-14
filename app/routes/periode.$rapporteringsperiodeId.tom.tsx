f0e3d2bd0ab5a81894b7d26ebc21ff8e8875b7d9
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-10-14 11:14:15 +0200

    Skriv tester for TomRapporteringsside

--- app/routes/periode.$rapporteringsperiodeId.tom.tsx
+++ app/routes/periode.$rapporteringsperiodeId.tom.tsx
@@ -1,13 +1,24 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
 import { Alert, Heading } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
-import { useNavigate } from "@remix-run/react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData, useNavigate } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { hentPeriode } from "~/models/rapporteringsperiode.server";
 import { useSanity } from "~/hooks/useSanity";
-import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import { RemixLink } from "~/components/RemixLink";
 
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  invariant(params.rapporteringsperiodeId, "params.rapporteringsperiode er påkrevd");
+
+  const periodeId = params.rapporteringsperiodeId;
+  const periode = await hentPeriode(request, periodeId, false);
+
+  return json({ periode });
+}
+
 export default function TomRapporteringsPeriodeSide() {
-  const { periode } = useTypedRouteLoaderData("routes/periode.$rapporteringsperiodeId");
+  const { periode } = useLoaderData<typeof loader>();
   const { getAppText, getRichText, getLink } = useSanity();
 
   const navigate = useNavigate();
---
+++ tests/routes/periode.rapporteringsperiodeId.tom.test.tsx
@@ -0,0 +1,101 @@
+import { createRemixStub } from "@remix-run/testing";
+import { render as TLRender, screen } from "@testing-library/react";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from "vitest";
+import { lagRapporteringsperiode } from "~/devTools/rapporteringsperiode";
+import TomRapporteringsPeriodeSide, { loader } from "~/routes/periode.$rapporteringsperiodeId.tom";
+import { createHandlers } from "../../mocks/handlers";
+import { withDb } from "../../mocks/responses/db";
+import { server } from "../../mocks/server";
+import { sessionRecord } from "../../mocks/session";
+import { endSessionMock, mockSession } from "../helpers/auth-helper";
+
+vi.mock("~/hooks/useSanity", () => ({
+  useSanity: () => ({
+    getAppText: (key: string) => key,
+    getLink: (key: string) => ({ linkText: key, href: key }),
+    getRichText: (key: string) => key,
+  }),
+}));
+
+vi.mock("@portabletext/react", () => ({
+  PortableText: ({ value }: { value: string }) => value,
+}));
+
+const testDb = withDb(sessionRecord.getDatabase("123"));
+const mockResponse = () => server.use(...createHandlers(testDb));
+
+beforeEach(() => {
+  testDb.clear();
+});
+
+describe("TomRapporteringsPeriodeSide", () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+  afterAll(() => server.close());
+  afterEach(() => {
+    server.resetHandlers();
+    endSessionMock();
+  });
+
+  const rapporteringsperiode = {
+    ...lagRapporteringsperiode({ kanSendes: true }),
+    periode: { fraOgMed: "2024-01-01", tilOgMed: "2024-01-14" },
+  };
+
+  const tomRapporteringsPeriodeSide = `/periode/${rapporteringsperiode.id}/tom`;
+
+  const render = () => {
+    const RemixStub = createRemixStub([
+      {
+        path: "/periode/:rapporteringsperiodeId/tom",
+        Component: TomRapporteringsPeriodeSide,
+        loader,
+        handle: {
+          params: { rapporteringsperiodeId: rapporteringsperiode.id },
+        },
+      },
+    ]);
+    TLRender(<RemixStub initialEntries={[tomRapporteringsPeriodeSide]} />);
+  };
+
+  test("Skal vise alert om at brukeren ikke har meldt noe ", async () => {
+    testDb.addRapporteringsperioder(rapporteringsperiode);
+
+    mockSession();
+    mockResponse();
+
+    render();
+
+    expect(
+      await screen.findByRole("heading", { name: /rapportering-tom-periode-tittel/ })
+    ).toBeInTheDocument();
+    expect(await screen.findByText(/rapportering-tom-periode-innhold/)).toBeInTheDocument();
+  });
+
+  test("Skal vise veiledning for neste steg", async () => {
+    testDb.addRapporteringsperioder(rapporteringsperiode);
+
+    mockSession();
+    mockResponse();
+
+    render();
+
+    expect(await screen.findByText(/rapportering-tom-noe-å-rapportere/)).toBeInTheDocument();
+    expect(await screen.findByText(/rapportering-tom-ingen-å-rapportere/)).toBeInTheDocument();
+  });
+
+  test("Skal vise tilbake- og nesteknapp", async () => {
+    testDb.addRapporteringsperioder(rapporteringsperiode);
+
+    mockSession();
+    mockResponse();
+
+    render();
+
+    expect(
+      await screen.findByRole("button", { name: /rapportering-periode-send-inn-tilbake/ })
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /rapportering-knapp-neste/ })
+    ).toBeInTheDocument();
+  });
+});
