a2b7173330e8dcd1f5207eff65aeb9ed038b4d23
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-15 08:42:29 +0200

    Legg til todo

--- app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
+++ app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
@@ -7,6 +7,7 @@ import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import { Kvittering } from "~/components/Kvittering";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  // TODO: Er listen med rapporteringsperioder med eller uten den innsendte perioden?
   const rapporteringsperioder = await hentRapporteringsperioder(request);
   const harNestePeriode = perioderSomKanSendes(rapporteringsperioder).length > 0;
 
