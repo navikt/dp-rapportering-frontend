2b93088bceb4abcdb319dbe6e0c67310b2412c00
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-15 08:19:31 +0200

    Bare vis "gå til neste meldekort" hvis det er et meldekort å sende inn

--- app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
+++ app/routes/periode.$rapporteringsperiodeId.bekreftelse.tsx
@@ -1,13 +1,14 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { hentRapporteringsperioder } from "~/models/rapporteringsperiode.server";
+import { perioderSomKanSendes } from "~/utils/periode.utils";
 import { useSanity } from "~/hooks/useSanity";
 import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import { Kvittering } from "~/components/Kvittering";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const rapporteringsperioder = await hentRapporteringsperioder(request);
-  const harNestePeriode = rapporteringsperioder.length > 0;
+  const harNestePeriode = perioderSomKanSendes(rapporteringsperioder).length > 0;
 
   return json({ harNestePeriode });
 }
