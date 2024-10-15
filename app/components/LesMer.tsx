9c58c8d1e0a223b08feb480f57d2984133c3b97f
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-15 11:59:53 +0200

    Åpne eksterne lenker i ny fane

--- app/components/LesMer.tsx
+++ app/components/LesMer.tsx
@@ -2,12 +2,37 @@ import { ReadMore } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
 import { useSanity } from "~/hooks/useSanity";
 
+const components = {
+  marks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    link: ({ value, children }: { value?: { blank: boolean; href: string }; children: any }) => {
+      // Read https://css-tricks.com/use-target_blank/
+      if (!value) return children;
+
+      const { blank, href } = value;
+
+      console.log(blank, href);
+
+      return blank ? (
+        <a href={href} target="_blank" rel="noreferrer">
+          {children}
+        </a>
+      ) : (
+        <a href={href}>{children}</a>
+      );
+    },
+  },
+};
+
 export function LesMer() {
   const { getRichText, getAppText } = useSanity();
   return (
     <div className="les-mer-container">
       <ReadMore header={getAppText("rapportering-les-mer-hva-skal-rapporteres-tittel")}>
-        <PortableText value={getRichText("rapportering-les-mer-hva-skal-rapporteres-innhold")} />
+        <PortableText
+          components={components}
+          value={getRichText("rapportering-les-mer-hva-skal-rapporteres-innhold")}
+        />
       </ReadMore>
     </div>
   );
