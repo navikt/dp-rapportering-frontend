4910cf303c1e2ef1c6277ebe867deb9cae8eacf3
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-15 13:13:24 +0200

    Legg til external link icon

--- app/components/LesMer.tsx
+++ app/components/LesMer.tsx
@@ -1,30 +1,7 @@
-import { ExternalLinkIcon } from "@navikt/aksel-icons";
 import { ReadMore } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
+import { getSanityPortableTextComponents } from "~/sanity/sanityPortableTextComponents";
 import { useSanity } from "~/hooks/useSanity";
-
-const components = {
-  marks: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    link: ({ value, children }: { value?: { blank: boolean; href: string }; children: any }) => {
-      // Read https://css-tricks.com/use-target_blank/
-      if (!value) return children;
-
-      const { blank, href } = value;
-
-      console.log(blank, href);
-
-      return blank ? (
-        <a href={href} target="_blank" rel="noreferrer">
-          {children}
-          <ExternalLinkIcon title="a11y-title" />
-        </a>
-      ) : (
-        <a href={href}>{children}</a>
-      );
-    },
-  },
-};
 
 export function LesMer() {
   const { getRichText, getAppText } = useSanity();
@@ -32,7 +9,7 @@ export function LesMer() {
     <div className="les-mer-container">
       <ReadMore header={getAppText("rapportering-les-mer-hva-skal-rapporteres-tittel")}>
         <PortableText
-          components={components}
+          components={getSanityPortableTextComponents()}
           value={getRichText("rapportering-les-mer-hva-skal-rapporteres-innhold")}
         />
       </ReadMore>
--- app/sanity/sanityPortableTextComponents.tsx
+++ app/sanity/sanityPortableTextComponents.tsx
@@ -1,13 +1,40 @@
-export function getSanityPortableTextComponents(replaceWithValue: {
-  [key: string]: string | number;
-}) {
+import { ExternalLinkIcon } from "@navikt/aksel-icons";
+import { PortableTextReactComponents } from "@portabletext/react";
+
+export function getSanityPortableTextComponents(
+  replaceWithValue: {
+    [key: string]: string | number;
+  } = {}
+): Partial<PortableTextReactComponents> {
   return {
+    marks: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      link: ({ value, children }: { value?: { blank: boolean; href: string }; children: any }) => {
+        // Read https://css-tricks.com/use-target_blank/
+        if (!value) return children;
+
+        const { blank, href } = value;
+
+        return blank ? (
+          <a href={href} target="_blank" rel="noreferrer">
+            {children}
+            <ExternalLinkIcon title="a11y-title" />
+          </a>
+        ) : (
+          <a href={href}>{children}</a>
+        );
+      },
+    },
     types: {
       dynamicFieldReference: ({
         value,
       }: {
         value: { dynamiskFelt: { textId: string; type: string } };
       }) => {
+        if (!replaceWithValue) {
+          return value.dynamiskFelt.textId;
+        }
+
         return replaceWithValue[value.dynamiskFelt.textId] ?? value.dynamiskFelt.textId;
       },
     },
