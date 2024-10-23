19d8a6dcf327770b9d8888fd7fb9c2c071178ac4
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-23 14:53:52 +0200

    Fullfør testene

--- tests/utils/innsendt.test.ts
+++ tests/utils/innsendt.test.ts
@@ -32,24 +32,53 @@ describe("sorterAktiviteter", () => {
   });
 
   test("tre meldekort der to har originalId skal sorteres i rekkefølgen av originalId -> id", () => {
-    // const meldekort1 = {
-    //   ...innsendtRapporteringsperioderResponse[0],
-    //   mottattDato: "2021-01-01",
-    //   id: "1",
-    // };
-    // const meldekort2 = {
-    //   ...innsendtRapporteringsperioderResponse[0],
-    //   mottattDato: "2021-01-01",
-    //   id: "2",
-    //   originalId: "1",
-    // };
-    // const meldekort3 = {
-    //   ...innsendtRapporteringsperioderResponse[0],
-    //   mottattDato: "2021-01-01",
-    //   id: "3",
-    //   originalId: "2",
-    // };
-    // const sorterte = sorterGrupper([meldekort1, meldekort2]);
-    // expect(sorterte).toEqual([meldekort3, meldekort2, meldekort1]);
+    const meldekort1 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-01",
+      id: "1",
+    };
+
+    const meldekort2 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-01",
+      id: "2",
+      originalId: meldekort1.id,
+    };
+
+    const meldekort3 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-01",
+      id: "3",
+      originalId: meldekort2.id,
+    };
+
+    const sorterte = sorterGrupper([meldekort1, meldekort2, meldekort3]);
+
+    expect(sorterte).toEqual([meldekort3, meldekort2, meldekort1]);
+  });
+
+  test("tre meldekort der to har originalId og med ulike mottattDato skal sorteres i rekkefølgen av mottattDato og originalId -> id", () => {
+    const meldekort1 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-01",
+      id: "1",
+    };
+
+    const meldekort2 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-02",
+      id: "2",
+    };
+
+    const meldekort3 = {
+      ...innsendtRapporteringsperioderResponse[0],
+      mottattDato: "2021-01-03",
+      id: "3",
+      originalId: meldekort1.id,
+    };
+
+    const sorterte = sorterGrupper([meldekort1, meldekort2, meldekort3]);
+
+    expect(sorterte).toEqual([meldekort3, meldekort2, meldekort1]);
   });
 });
