ca3350d7fb7e1ab104f9e82a5a0991e73773f761
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-10-31 16:17:39 +0100

    Legg til corralationId i headers

--- app/utils/fetch.utils.ts
+++ app/utils/fetch.utils.ts
@@ -1,15 +1,25 @@
 import { getRapporteringOboToken } from "./auth.utils.server";
 import { isLocalOrDemo } from "./env.utils";
+import { uuidv7 } from "uuidv7";
 import { getSessionId } from "~/../mocks/session";
 import { IHttpProblem } from "~/utils/types";
 
+function generateCorralationId() {
+  // https://github.com/navikt/dp-rapportering-frontend/pull/242#pullrequestreview-2403834306
+  // korralasjon_id i dp-rappoortering kan være på maks 54 tegn
+  return `dp-rapp-${uuidv7()}`.substring(0, 54);
+}
+
 export async function getHeaders(request: Request, customHeaders = {}) {
   const onBehalfOfToken = await getRapporteringOboToken(request);
+
+  const corralationId = request.headers.get("X-Request-ID") ?? generateCorralationId();
 
   const headers = {
     "Content-Type": "application/json",
     Accept: "application/json",
     Authorization: `Bearer ${onBehalfOfToken}`,
+    "X-Request-ID": corralationId,
     ...customHeaders,
   };
 
