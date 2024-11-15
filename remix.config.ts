98ff59000213b963b4a247cc3785636b6235975a
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-11-15 14:45:29 +0100

    msw 2.4

--- remix.config.ts
+++ remix.config.ts
@@ -5,3 +5,6 @@
 // };
 
 // export default config;
+
+// https://meldekort.ekstern.dev.nav.no/arbeid/dagpenger/meldekort/assets/Accordion-DfKaHPuN.js
+// https://cdn.nav.no/teamdagpenger/dp-rapportering-frontend/static/client/assets/Accordion-DfKaHPuN.js
