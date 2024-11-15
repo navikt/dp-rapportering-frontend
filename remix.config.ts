19415177c5e02d6bf0e9bb60844486791bc4a8de
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-11-15 14:50:09 +0100

    publicPath

--- remix.config.ts
+++ remix.config.ts
@@ -1,10 +1,10 @@
-// import { AppConfig } from "@remix-run/dev";
+import { AppConfig } from "@remix-run/dev";
 
-// const config: AppConfig = {
-//   publicPath: "/arbeid/dagpenger/meldekort/",
-// };
+const config: AppConfig = {
+  publicPath: "https://cdn.nav.no/teamdagpenger/dp-rapportering-frontend/static/client/",
+};
 
-// export default config;
+export default config;
 
 // https://meldekort.ekstern.dev.nav.no/arbeid/dagpenger/meldekort/assets/Accordion-DfKaHPuN.js
 // https://cdn.nav.no/teamdagpenger/dp-rapportering-frontend/static/client/assets/Accordion-DfKaHPuN.js
