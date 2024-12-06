25739d22297bc1967eb02d8f48fdd7584b5bb96b
Igor Weber <igor.shuliakov@nav.no>
2024-12-06 15:18:57 +0100

    Eslint ignore server/build

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import typescript from "typescript-eslint";
 export default [
   {
     name: "ignore",
-    ignores: ["graphql/","build/", "coverage/", "playwright-report/"],
+    ignores: ["graphql/", "build/", "coverage/", "server/build/", "playwright-report/"],
   },
   {
     name: "eslint/recommended:",
