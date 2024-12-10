49e607f25c71bbf324670ebcae494a61584e8ce6
Igor Weber <igor.shuliakov@nav.no>
2024-12-10 08:59:42 +0100

    Skriv ut knappen

--- app/components/Kvittering.tsx
+++ app/components/Kvittering.tsx
@@ -15,6 +15,7 @@ import {
   RegistertArbeidssokerAlert,
 } from "./arbeidssokerregister/ArbeidssokerRegister";
 import { NavigasjonContainer } from "./navigasjon-container/NavigasjonContainer";
+import { PrinterSmallFillIcon } from "@navikt/aksel-icons";
 
 interface Ikvittering {
   tittel: string;
@@ -56,10 +57,21 @@ export function Kvittering({ tittel, periode, harNestePeriode }: Ikvittering) {
             ) : (
               <AvregistertArbeidssokerAlert />
             )}
+
+            <div className={styles.skrivUtKnappen}>
+              <Button
+                variant="tertiary"
+                icon={<PrinterSmallFillIcon aria-hidden />}
+                onClick={() => window.print()}
+              >
+                Skriv ut
+              </Button>
+            </div>
           </Accordion.Content>
         </Accordion.Item>
       </Accordion>
       <div data-uxsignals-embed="panel-ppugndwzu6" style={{ margin: "var(--a-spacing-8) auto" }} />
+
       <NavigasjonContainer>
         {harNestePeriode ? (
           <RemixLink
