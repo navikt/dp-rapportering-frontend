99ddca3226dee20f426a9781d91d50faefe5effe
Sandra Elise Eng <117898702+Zandra7@users.noreply.github.com>
2025-01-21 14:09:09 +0100

    Readmore filtrering (#373)
    Legger til filtrering på les mer
    ---------
    
    Co-authored-by: Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>

--- app/components/LesMer.tsx
+++ app/components/LesMer.tsx
@@ -1,8 +1,10 @@
-import { ReadMore } from "@navikt/ds-react";
+import { Checkbox, CheckboxGroup, ReadMore } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
+import { useState } from "react";
 
 import { useAmplitude } from "~/hooks/useAmplitude";
 import { useSanity } from "~/hooks/useSanity";
+import { AktivitetType, aktivitetTypeMap } from "~/utils/aktivitettype.utils";
 
 import styles from "../styles/lesMer.module.css";
 
@@ -10,9 +12,30 @@ interface IProps {
   periodeId: string;
 }
 
+export const lesMerInnhold = [
+  {
+    value: AktivitetType.Arbeid,
+    content: "rapportering-les-mer-hva-skal-rapporteres-innhold-jobb",
+  },
+  {
+    value: AktivitetType.Syk,
+    content: "rapportering-les-mer-hva-skal-rapporteres-innhold-syk",
+  },
+  {
+    value: AktivitetType.Fravaer,
+    content: "rapportering-les-mer-hva-skal-rapporteres-innhold-ferie",
+  },
+  {
+    value: AktivitetType.Utdanning,
+    content: "rapportering-les-mer-hva-skal-rapporteres-innhold-utdanning",
+  },
+];
+
 export function LesMer({ periodeId }: IProps) {
   const { getRichText, getAppText } = useSanity();
   const { trackAccordionApnet, trackAccordionLukket } = useAmplitude();
+
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
 
   const tekstId = "rapportering-les-mer-hva-skal-rapporteres-tittel";
   const header = getAppText(tekstId);
@@ -25,10 +48,30 @@ export function LesMer({ periodeId }: IProps) {
     }
   }
 
+  function handleCheckboxChange(values: string[]) {
+    setSelectedValues(values);
+  }
+
+  const filtrertInnhold = lesMerInnhold.filter(
+    (element) => selectedValues.length === 0 || selectedValues.includes(element.value),
+  );
+
   return (
     <div className={styles.container}>
       <ReadMore header={header} onOpenChange={trackOpenChange}>
-        <PortableText value={getRichText("rapportering-les-mer-hva-skal-rapporteres-innhold")} />
+        <CheckboxGroup
+          legend={getAppText("rapportering-les-mer-hva-skal-rapporteres-legend")}
+          onChange={handleCheckboxChange}
+        >
+          {lesMerInnhold.map((element) => (
+            <Checkbox key={element.value} value={element.value}>
+              {aktivitetTypeMap(element.value, getAppText)}
+            </Checkbox>
+          ))}
+        </CheckboxGroup>
+        {filtrertInnhold.map((element) => (
+          <PortableText key={element.value} value={getRichText(element.content)} />
+        ))}
       </ReadMore>
     </div>
   );
