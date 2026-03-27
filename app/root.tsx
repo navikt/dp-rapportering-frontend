650d1ae243dfa1aadc33757787569f0b9e37d90c
Igor Weber <igor.weber@nav.no>
2026-03-27 21:09:51 +0100

    Fjernet redirect til felles meldekort-løsningen

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,6 @@ import { useInjectDecoratorScript } from "./hooks/useInjectDecoratorScript";
 import { useSanity } from "./hooks/useSanity";
 import { useTypedRouteLoaderData } from "./hooks/useTypedRouteLoaderData";
 import { getLanguage, setLanguage } from "./models/language.server";
-import { harDpMeldeplikt } from "./models/rapporteringsperiode.server";
 import { sanityConfig } from "./sanity/sanity.config";
 import { allTextsQuery } from "./sanity/sanity.query";
 import type { ISanity } from "./sanity/sanity.types";
@@ -109,12 +108,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
         "Set-Cookie": `sessionId=${uuidv7()}; Path=/; SameSite=Lax`,
       },
     });
-  }
-
-  const brukerHarDpMeldeplikt = await harDpMeldeplikt(request);
-
-  if (!brukerHarDpMeldeplikt) {
-    return redirect(`${getEnv("FELLES_MELDEKORT_URL")}/meldekort`);
   }
 
   return {
