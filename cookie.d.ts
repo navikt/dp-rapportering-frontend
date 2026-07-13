8d8b0b7d5be9904f23e8bc05d3177aad32da6b6a
Igor Weber <igor.weber@nav.no>
2026-07-13 12:22:46 +0200

    Erstattet deprecated metoder

--- cookie.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "cookie" {
-  export function parse(str: string, options?: Record<string, unknown>): Record<string, string>;
-  export function serialize(name: string, value: string, options?: Record<string, unknown>): string;
-}
